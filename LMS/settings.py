7f7c3241bf5c8e9563fa19a6aecec00f66329b89
Harieesh Rakhavandaran N A <110871194+Harieesht@users.noreply.github.com>
2024-06-25 12:45:23 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -150,11 +150,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     # Add more directories if necessary
 ]
-# STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
