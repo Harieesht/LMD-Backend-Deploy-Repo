00a40c32d499e78187cffe2f496d75356901465a
Harieesh Rakhavandaran N A <110871194+Harieesht@users.noreply.github.com>
2024-06-25 15:17:30 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://localhost:5173']
 
 
 # Application definition
